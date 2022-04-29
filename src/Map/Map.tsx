4f26a940b0c048e096189ed30078c588b2193bb1
KABIKA681 <pascalkabikampan@gmail.com>
2022-04-29 12:49:21 +0200

    Add function control th map

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, {useRef, useEffect, useState} from "react";
 
 interface IMap {
     mapType:google.maps.MapTypeId,
     mapTypeControl?: boolean;
 }
 
+type GoogleLatLng = google.maps.LatLng;
+type GoogleMap = google.maps.Map;
+
 const Map: React.FC<IMap> = ({mapType, mapTypeControl = false}) => {
+
+    const ref = useRef<HTMLDivElement>(null);
+    const [map, setMap] = useState<GoogleMap>();
+
+    const startMap = ():void => {
+        if (!map) {
+            defaultMapStart()
+        }
+    }
+    useEffect(startMap, [map]);
+
+    const defaultMapStart = ():void => {
+        const defaulAddress = new google.maps.LatLng(-1.940278, 29.873888);
+        initMap(5, defaulAddress);
+    }
+
+    const initMap = (zoomLevel: number, address: GoogleLatLng): void => {
+        if (ref.current) {
+            setMap(
+            new google.maps.Map(ref.current, {
+                zoom: zoomLevel,
+                center: address,
+                mapTypeControl: mapTypeControl,
+                streetViewControl: false,
+                zoomControl: true,
+                mapTypeId: mapType
+            })
+            )
+        }
+    };
+
     return (
-        <div>My Map</div>
+        <div className="map-container">
+            <div ref={ref} className="map-container__map">
+
+            </div>
+        </div>
     );
 };
 export default Map;  
