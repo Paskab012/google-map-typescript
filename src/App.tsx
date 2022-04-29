f4678164900bf65009d30da1a6d52634bf422763
KABIKA681 <pascalkabikampan@gmail.com>
2022-04-29 12:12:06 +0200

    Load the map with its props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Map from './Map';
+import { loadMapApi } from './utils/GoogleMapsUtils';
 
 function App() {
   const [scriptLoaded, setScriptLoaded] = useState(false);
   
+  useEffect(() => {
+    const googleMapScript = loadMapApi();
+    googleMapScript.addEventListener("load", function() {
+      setScriptLoaded(true);
+    })
+  }, []);
   
   return (
     <div className="App">
-      Hello
+      {scriptLoaded && (
+        <Map mapType={google.maps.MapTypeId.ROADMAP} mapTypeControl={true} />
+      )}
     </div>
   );
 }
--- src/utils/GoogleMapsUtils.ts
+++ src/utils/GoogleMapsUtils.ts
@@ -1,6 +1,6 @@
 export const loadMapApi = () => {
     const mapsURL = `https://maps.googleapis.com/maps/api/js?key=&libraries=places&language=eng&v=quarterly`;
-    const scripts = document.getElementsByTagName('scripts');
+    const scripts = document.getElementsByTagName('script');
 
     for(let i = 0; i< scripts.length; i++) {
         if (scripts[i].src.indexOf(mapsURL) === 0) {
